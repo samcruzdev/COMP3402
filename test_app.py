1d49f43969364a357dec3c742d12257c5b58bd7d
Samuel Cruz <contact@samcruz.dev>
2026-03-19 15:48:57 -0400

    Agregar nuevos tests para la nueva tarea de devops

--- test_app.py
+++ test_app.py
@@ -1,5 +1,33 @@
-from app import saludar
+from app import autenticar_usuario, saludar
 
 
 def test_saludar():
     assert saludar("Ana") == "Hola, Ana"
+
+
+def test_login_exitoso():
+    resultado = autenticar_usuario("admin", "1234", 2.14)
+    assert resultado["success"] == True
+    assert resultado["error_id"] == ""
+    assert resultado["response_time_ms"] < 500
+
+
+def test_usuario_inexistente():
+    resultado = autenticar_usuario("pedro", "1234", 3.05)
+    assert resultado["success"] == False
+    assert resultado["error_id"] == "user-not-exist"
+    assert resultado["response_time_ms"] < 500
+
+
+def test_contrasena_incorrecta():
+    resultado = autenticar_usuario("pedro", "1234", 620)
+    assert resultado["success"] == False
+    assert resultado["error_id"] == "user-not-exist"
+    assert resultado["response_time_ms"] < 500
+
+
+def test_empty_fields():
+    resultado = autenticar_usuario("pedro", "1234", 1.90)
+    assert resultado["success"] == False
+    assert resultado["error_id"] == "empty-fields"
+    assert resultado["response_time_ms"] < 500
